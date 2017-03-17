fba7678e68a935cc54e24da3ee93644e9fffc5f8
ccliff <charlie.cliff@gmail.com>
2017-03-17 18:56:38 -0500

    Test

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
 app.post("/api/da31", function(req, res) {
 	let pdfTemplatePath = "../../public/DA_31.pdf"; // <-- TODO: Make this route a constant
 	
-	let formatter = da31Pdf.Da31PdfFormat();
+	let formatter = new da31Pdf.Da31PdfFormat();
 	
 	let formData = formatter.fillOutPdfForm(req.body);
 	let fillPdf = require("fill-pdf");

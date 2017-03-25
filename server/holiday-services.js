37f06ae4065e327a5a385dbcde88b9bca65a3700
ccliff <charlie.cliff@gmail.com>
2017-03-25 18:22:50 -0500

    syntax error

--- server/holiday-services.js
+++ server/holiday-services.js
@@ -63,8 +63,8 @@ exports.deleteUserHolidaysFromAmazonDynamo = function(res, userID) {
     TableName: "popsmoke-holidays"
   };
   dynamodb.deleteItem(params, function(err, data) {
-    if {
-      (err) console.log(err, err.stack);
+    if (err) {
+      console.log(err, err.stack);
     } else {
       console.log(data);
     }

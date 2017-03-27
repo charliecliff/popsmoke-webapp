d406dced6ea178b24c31f2ef906dd7f8c6324f29
ccliff <charlie.cliff@gmail.com>
2017-03-26 23:35:08 -0500

    man this is fucking stupid

--- server/user-services.js
+++ server/user-services.js
@@ -1,10 +1,7 @@
 var express = require('express');
-  var AWS = require("aws-sdk");
-  AWS.config.update({ accessKeyId: "AKIAIDMIESKUD4F657BQ", 
-                      secretAccessKey: "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",
-                      region:'us-east-1'});      
-  var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-  
+
+
+
 var exports = module.exports = {};
 
 exports.parameters = {
@@ -29,13 +26,13 @@ exports.putUserToAmazonDynamo = function(req, res) {
 }
 
 exports.postUserToAmazonDynamo = function(req, res) {
-  // var AWS = require("aws-sdk");
-  // AWS.config.update({ accessKeyId: "AKIAIDMIESKUD4F657BQ", 
-  //                     secretAccessKey: "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",
-  //                     region:'us-east-1'});      
-  // var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  var AWS = require("aws-sdk");
+  AWS.config.update({ accessKeyId: "AKIAIDMIESKUD4F657BQ", 
+                      secretAccessKey: "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",
+                      region:'us-east-1'});      
+  var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-    var userMap = this.buildAWSMapFromUserRequestBody(req.body);
+  var userMap = this.buildAWSMapFromUserRequestBody(req.body);
   var userModel = this.buildModelFromUserRequestBody(req.body);
   var params = { Item: userMap,
                  ReturnConsumedCapacity: "TOTAL", 

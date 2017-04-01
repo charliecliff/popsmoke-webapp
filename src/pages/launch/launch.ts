c3ff8b40acaac13d8b4340e4accdc6b532c33e55
ccliff <charlie.cliff@gmail.com>
2017-03-31 21:30:21 -0500

    Test

--- src/pages/launch/launch.ts
+++ src/pages/launch/launch.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { AngularFire } from "angularfire2";
-
-import * as MenuConstants from '../../config/menuConstants';
-
-import { User } from '../../models/User';
-import { Menu, MenuOption } from '../../models/Menu';
 
 import * as Reducers from '../../reducers';
-
+import { User } from '../../models/User';
 import { MainTabPage } from '../main-tab/main-tab';
 import { LoginPage } from '../login/login';
 
@@ -20,27 +14,21 @@ import { LoginPage } from '../login/login';
 })
 export class LaunchPage {
 
-  public currentMenu: Menu;
-  public currentUser: User;
-
   constructor(public navCtrl: NavController, 
-  						public modalCtrl: ModalController, 
-  						public angularFire: AngularFire,
               public store: Store<Reducers.AppState>) { }
 	
 	ngOnInit() {
     this.store.select('user').subscribe(user => {
-      this.currentUser = user as User;
-      this.pushController(this.currentUser, this.currentMenu);
+      this.pushController(user as User);
     });
 	}
 
-  private pushController(user, menu) {
+  private pushController(user) {
     this.navCtrl.popToRoot();
-    // if ( user.userID == undefined ) {
-    // this.navCtrl.push(LoginPage);
-    // } else {
+    if ( user.userID == undefined ) {
+      this.navCtrl.push(LoginPage);
+    } else {
       this.navCtrl.push(MainTabPage);
-    // }
+    }
   }
 }

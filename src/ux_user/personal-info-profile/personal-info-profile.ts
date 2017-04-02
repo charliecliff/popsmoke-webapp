bc5e02613f89c620f4f1fea95d82a625a515640d
ccliff <charlie.cliff@gmail.com>
2017-04-02 16:25:00 -0500

    test

--- src/ux_user/personal-info-profile/personal-info-profile.ts
+++ src/ux_user/personal-info-profile/personal-info-profile.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, trigger, state, style, transition, animate} from '@angular/core';
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 import { Store } from '@ngrx/store';
 
 import * as Constants from '../constants';
@@ -9,15 +10,36 @@ import { AppState } from '../../reducers';
 import { User } from '../../models/User';
 import { PersonalInfo } from '../../models/PersonalInfo';
 
+
+import { StationProfilePage } from '../station-profile/station-profile';
+
 @Component({
   selector: 'page-personal-info-profile',
-  templateUrl: 'personal-info-profile.html'
+  templateUrl: 'personal-info-profile.html',
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class PersonalInfoProfilePage {
 
+
+
+menuState:string = 'out';
+
+
+
   public updateButtonCopy = Constants.UPDATE_BUTTON;
 
-	PAGE_TITLE	: string					= "PERSONAL INFORMATION";
+	PAGE_TITLE	: string    = "PERSONAL INFORMATION";
 	
 	first_name: string			= "FIRST NAME";
 	last_name: string				= "LAST NAME";
@@ -26,11 +48,12 @@ export class PersonalInfoProfilePage {
 	rank: string						= "RANK";
 	phone	: string					= "PHONE";
 
-  personalInfoForm: FormGroup;
+  // personalInfoForm: FormGroup;
   
   constructor(private formBuilder: FormBuilder, 
               private navCtrl: NavController, 
-              private store: Store<AppState>) {
+              private store: Store<AppState>,
+              public modalCtrl: ModalController) {
     this.store.select("user").take(1)
                              .subscribe(this.initialUserCallback);
   }
@@ -40,25 +63,36 @@ export class PersonalInfoProfilePage {
   }
 
   submit() { 
-    let personalInfo = {} as PersonalInfo;
-    personalInfo.firstName = this.personalInfoForm.value.firstName;
-    personalInfo.lastName = this.personalInfoForm.value.lastName;
-    personalInfo.middleInitial = this.personalInfoForm.value.middleInitial;
-    personalInfo.ssn = this.personalInfoForm.value.ssn;
-    personalInfo.rank = this.personalInfoForm.value.rank;
-    personalInfo.phoneNumber = this.personalInfoForm.value.phoneNumber;
-    this.navCtrl.popToRoot();
+    // let personalInfo = {} as PersonalInfo;
+    // personalInfo.firstName = this.personalInfoForm.value.firstName;
+    // personalInfo.lastName = this.personalInfoForm.value.lastName;
+    // personalInfo.middleInitial = this.personalInfoForm.value.middleInitial;
+    // personalInfo.ssn = this.personalInfoForm.value.ssn;
+    // personalInfo.rank = this.personalInfoForm.value.rank;
+    // personalInfo.phoneNumber = this.personalInfoForm.value.phoneNumber;
+    // this.navCtrl.popToRoot();
+  }
+
+  selectProfileField()  {
+    console.log("selectProfileField");
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+
+     // let profileModal = this.modalCtrl.create(StationProfilePage);
+     // profileModal.onDidDismiss(data => {
+     //   console.log(data);
+     // });
+     // profileModal.present();
   }
 
   private initialUserCallback = (user) => {
-    var personalInfo = user.personalInfo;
-    this.personalInfoForm = this.formBuilder.group({
-      firstName: [personalInfo.firstName],
-      middleInitial: [personalInfo.middleInitial],
-      lastName: [personalInfo.lastName],
-      ssn: [personalInfo.ssn],
-      rank: [personalInfo.rank],
-      phone: [personalInfo.phone]
-    });
+    // var personalInfo = user.personalInfo;
+    // this.personalInfoForm = this.formBuilder.group({
+    //   firstName: [personalInfo.firstName],
+    //   middleInitial: [personalInfo.middleInitial],
+    //   lastName: [personalInfo.lastName],
+    //   ssn: [personalInfo.ssn],
+    //   rank: [personalInfo.rank],
+    //   phone: [personalInfo.phone]
+    // });
   }
 }

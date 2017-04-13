e99147f0351af91a5850f36e392499447df2eb0a
ccliff <charlie.cliff@gmail.com>
2017-04-13 07:34:42 -0500

    test

--- src/providers/da31-provider.ts
+++ src/providers/da31-provider.ts
@@ -44,16 +44,19 @@ export class DA31Provider {
     body[DA31Constants.CITY]              = packet.destination.city;
     body[DA31Constants.STATE]             = packet.destination.state;
     body[DA31Constants.ZIP]               = packet.destination.zip;
+
     body[DA31Constants.TYPE_OF_LEAVE]     = packet.leave.leaveType;
-
-    
     body[DA31Constants.LEAVE_EXPLANATION] = packet.leave.leaveExplanation;
-    body[DA31Constants.ACCRUED_LEAVE]     = packet.leave.accruedLeave;
-    body[DA31Constants.REQUESTED_LEAVE]   = packet.leave.requestedLeave;
-    body[DA31Constants.ADVANCED_LEAVE]    = packet.leave.advancedLeave;
-    body[DA31Constants.EXCESS_LEAVE]      = packet.leave.excessLeave;
 
-    
+    body[DA31Constants.ACCRUED_LEAVE] = packet.leave.accruedLeave == undefined 
+      ? "0" : packet.leave.accruedLeave;
+    body[DA31Constants.REQUESTED_LEAVE] = packet.leave.requestedLeave == undefined 
+      ? "0" : packet.leave.requestedLeave;
+    body[DA31Constants.ADVANCED_LEAVE] = packet.leave.advancedLeave == undefined 
+      ? "0" : packet.leave.advancedLeave;
+    body[DA31Constants.EXCESS_LEAVE] = packet.leave.excessLeave == undefined 
+      ? "0" : packet.leave.excessLeave;
+      
     // body[DA31Constants.LEAVE_START_DATE] = packet.leave.startDate;
 
     // body[DA31Constants.LEAVE_END_DATE]   = packet.leave.endDate;

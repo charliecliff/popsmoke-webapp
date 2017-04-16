a7db7c030a07eba712b15756bc8385ba11def288
ccliff <charlie.cliff@gmail.com>
2017-04-15 22:55:29 -0500

    Test

--- server/file-services.js
+++ server/file-services.js
@@ -8,23 +8,23 @@ var exports = module.exports = {};
 
 exports.parseFileWithIDFromUploadRequest = function(req, id, callback) {
   console.log("parseFileWithIDFromUploadRequest");
+  console.log(path.join(__dirname, '/uploads'));
+
   var form = new formidable.IncomingForm();
   form.multiples = true;
   form.uploadDir = path.join(__dirname, '/uploads');
 
-  
-  let fileName = form.name;
-  console.log("Uploaded File Name:" + fileName);
-
   form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    console.log('file');
+    fs.rename(file.path, path.join(form.uploadDir, file.name) );
   });
   form.on('error', function(err) {    
     console.log('An error has occured: \n' + err);
     callback(err);
   });
   form.on('end', function() {
-    awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
+    console.log('end');
+    // awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
   });
   form.parse(req);
 }

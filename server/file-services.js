ce4fdf642b42bbeb189a6d019af8d5dd23b1a75e
ccliff <charlie.cliff@gmail.com>
2017-04-15 23:48:53 -0500

    Test

--- server/file-services.js
+++ server/file-services.js
@@ -17,7 +17,12 @@ exports.parseFileWithIDFromUploadRequest = function(req, id, callback) {
     console.log("on file");
     console.log("name: " + file.name);
     console.log("path: " + file.path);
-    awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
+
+    let fileName = path.join(file.path, file.name);
+    console.log("new file name: " + fileName);
+
+
+    awsServices.uploadJPEGFileAtPathToAmazonS3(fileName, callback);
 
 
     // fs.rename(file.path, path.join(form.uploadDir, file.name), function (err) {

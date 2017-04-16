3d5865be191c45be6d2cf9e2346065c870d20d1e
ccliff <charlie.cliff@gmail.com>
2017-04-15 23:03:35 -0500

    Test

--- server/file-services.js
+++ server/file-services.js
@@ -8,23 +8,23 @@ var exports = module.exports = {};
 
 exports.parseFileWithIDFromUploadRequest = function(req, id, callback) {
   console.log("parseFileWithIDFromUploadRequest");
-  console.log(path.join(__dirname, '/uploads'));
+  console.log(path.join(__dirname, 'public//uploads'));
 
   var form = new formidable.IncomingForm();
   form.multiples = true;
-  form.uploadDir = path.join(__dirname, '/uploads');
+  form.uploadDir = 'public/uploads';
 
-  // form.on('file', function(field, file) {
-  //   console.log('file');
-  //   fs.rename(file.path, path.join(form.uploadDir, file.name) );
-  // });
-  // form.on('error', function(err) {    
-  //   console.log('An error has occured: \n' + err);
-  //   callback(err);
-  // });
-  // form.on('end', function() {
-  //   console.log('end');
-  //   // awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
-  // });
-  // form.parse(req);
+  form.on('file', function(field, file) {
+    console.log('file');
+    fs.rename(file.path, path.join(form.uploadDir, file.name) );
+  });
+  form.on('error', function(err) {    
+    console.log('An error has occured: \n' + err);
+    // callback(err);
+  });
+  form.on('end', function() {
+    console.log('end');
+    awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
+  });
+  form.parse(req);
 }

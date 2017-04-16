64698577a5f3c439e90dce0572ff5300437df611
ccliff <charlie.cliff@gmail.com>
2017-04-15 22:58:52 -0500

    Testing

--- server/file-services.js
+++ server/file-services.js
@@ -14,17 +14,17 @@ exports.parseFileWithIDFromUploadRequest = function(req, id, callback) {
   form.multiples = true;
   form.uploadDir = path.join(__dirname, '/uploads');
 
-  form.on('file', function(field, file) {
-    console.log('file');
-    fs.rename(file.path, path.join(form.uploadDir, file.name) );
-  });
-  form.on('error', function(err) {    
-    console.log('An error has occured: \n' + err);
-    callback(err);
-  });
-  form.on('end', function() {
-    console.log('end');
-    // awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
-  });
-  form.parse(req);
+  // form.on('file', function(field, file) {
+  //   console.log('file');
+  //   fs.rename(file.path, path.join(form.uploadDir, file.name) );
+  // });
+  // form.on('error', function(err) {    
+  //   console.log('An error has occured: \n' + err);
+  //   callback(err);
+  // });
+  // form.on('end', function() {
+  //   console.log('end');
+  //   // awsServices.uploadJPEGFileAtPathToAmazonS3(file.path, callback);
+  // });
+  // form.parse(req);
 }

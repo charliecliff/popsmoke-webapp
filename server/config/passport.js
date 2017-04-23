e56b2220b337baac79eddd9a4c5efb762097d91d
ccliff <charlie.cliff@gmail.com>
2017-04-23 11:06:30 -0500

    test

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,13 +1,23 @@
 var express       = require('express');
-var userService   = require("../user-services");
 var passport      = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var userService   = require("../user-services");
 
 //------------------------------------------------------------------------------
 // PUBlIC INTERFACE
 //------------------------------------------------------------------------------
 
 module.exports = function(passport) {
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.userID);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    userService.getUserWithPhoneNumber(phoneNumber, function(err, user) {
+      done(err, user);
+    });
+  });
 
   passport.use('local-login', new LocalStrategy({
         usernameField : 'userID',
@@ -16,17 +26,26 @@ module.exports = function(passport) {
     },
     function(username, password, done) {
       
+      console.log("local-login");
       let phoneNumber = username;
       userService.getUserWithPhoneNumber(phoneNumber, function(err, user) {
-        if (err) { 
+        if (err) {
+
+                console.log("local-login: err");
           return done(err); 
         }
         if (!user) {
+                console.log("local-login username");
+
           return done(null, false, { message: 'Incorrect username.' });
         }
         if (!user.validPassword(password)) {
+                console.log("local-login password");
+
           return done(null, false, { message: 'Incorrect password.' });
         }
+              console.log("local-login success");
+
         return done(null, user);
       });
     })

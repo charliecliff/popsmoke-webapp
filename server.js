d3bb3d25d5f8a4b8fafca1b8985586d04ea86df5
ccliff <charlie.cliff@gmail.com>
2017-04-23 11:03:18 -0500

    test

--- server.js
+++ server.js
@@ -93,17 +93,11 @@ app.post("/auth/logout", function(req, res) {
 });
 
 
-app.post('/signup', passport.authenticate('local-login', 
-  function (err, account) {
-    req.logIn(account, function() {
-    
-      console.log("DID GET CALLBACK");
-      res.status(200);
-      // res.status(err ? 500 : 200).send(err ? err : account);
-    
-    });
-  })(this.req, this.res, this.next)
-);
+app.post('/signup', passport.authenticate('local-login', {}),
+  function(req,res){
+    console.log('In Login');    
+    res.send(req.session);
+});
 
 //------------------------------------------------------------------------------
 // HELPER FUNCTIONS

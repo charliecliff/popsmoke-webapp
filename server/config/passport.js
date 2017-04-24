11a0cc1572bc3d5a32709200c19f6ade7119c8db
ccliff <charlie.cliff@gmail.com>
2017-04-23 19:25:06 -0500

    Removing Logging Statements

--- server/config/passport.js
+++ server/config/passport.js
@@ -26,30 +26,18 @@ module.exports = function(passport) {
     },
     function(req, userID, password, done) {
       
-      console.log("local");
-
       let phoneNumber = userID;
       
       userService.getUserWithPhoneNumber(phoneNumber, function(err, user) {
-        
-        console.log("iser: " + JSON.stringify(user) );
-
         if (err) {
-          console.log("local-login: err");
           return done(err); 
         }
         if (!user) {
-                console.log("local-login username");
-
           return done(null, false, { message: 'Incorrect username.' });
         }
         if (!(user.password == password)) {
-                console.log("local-login password");
-
           return done(null, false, { message: 'Incorrect password.' });
         }
-              console.log("local-login success");
-
         return done(null, user);
       });
     })

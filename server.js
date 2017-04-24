b7bafcd7afeda40d306c5c476d0eac26ca2c43f7
ccliff <charlie.cliff@gmail.com>
2017-04-24 00:10:46 -0500

    Test

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(session({
   secret: 'eg[isfd-8yF9-7w2315df{}+Ijsli;;to8',
   duration: 30 * 60 * 1000,
   activeDuration: 5 * 60 * 1000,
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());

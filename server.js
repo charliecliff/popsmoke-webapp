fcad3e33afe515625b81f0c5813ccb39139679fe
ccliff <charlie.cliff@gmail.com>
2017-04-25 17:50:09 -0500

    Testing the Set-Cookie

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
   
   res.header("Access-Control-Allow-Origin", "http://localhost:8100");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Expose-Headers", "Accept-Ranges, Content-Encoding, Content-Length, Content-Range, Cookies");
+  res.header("Access-Control-Expose-Headers", "Accept-Ranges, Content-Encoding, Content-Length, Content-Range, Set-Cookie");
 
   // res.header("Access-Control-Expose-Headers", true);
   res.header('Access-Control-Allow-Methods', "OPTIONS,GET,PUT,POST,DELETE");

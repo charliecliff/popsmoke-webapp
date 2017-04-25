ba00ad454a07b0bbd7a6172757e68e6b4275e7a8
ccliff <charlie.cliff@gmail.com>
2017-04-25 01:44:33 -0500

    res.header("Access-Control-Allow-Origin", null);

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ require('./server/config/passport')(passport); // pass passport for configuratio
 //------------------------------------------------------------------------------
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", null);
   res.header('Access-Control-Allow-Methods', "OPTIONS,GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Set-Cookie, Accept");
   res.header( "Access-Control-Expose-Headers", true);

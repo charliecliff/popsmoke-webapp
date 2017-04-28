1e1676493b9b3899f12929f34d257dcd7e5a799c
ccliff <charlie.cliff@gmail.com>
2017-04-27 19:38:51 -0500

    New Test

--- server/trips-services.js
+++ server/trips-services.js
@@ -5,14 +5,18 @@ var express = require('express');
 var exports = module.exports = {};
 
 exports.generateDocument = function(req, res, pdfFilePath) {
-  let pdfTemplatePath = "public/TRIPS.pdf";
-  let destinationPDF = pdfFilePath;
 
-  pdfFiller.generateFieldJson(destinationPDF, null, function(err, fdfData) {
-
-    console.log(fdfData);
-
-    fdfData.length.should.equal(0);
-    done();
+  let fs = require('fs'),
+  PDFParser = require("pdf2json");
+ 
+  let pdfParser = new PDFParser();
+ 
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    console.log("PARSE PDF FILE:");
+    console.log(JSON.stringify(pdfData));
   });
+ 
+  pdfParser.loadPDF("public/TRIPS.pdf"); 
+
 }
